b9709d5612a0bbd61b6f7b58318dc75881d2aa4f
dongwoo kang <ehddn3821@naver.com>
2022-07-01 23:04:19 +0900

    feat: Connect viewModels to controllers

---
+++ RxMemo/ViewModel/MemoComposeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MemoComposeViewModel.swift
+//  RxMemo
+//
+//  Created by dwKang on 2022/07/01.
+//
+
+import Foundation
+
+class MemoComposeViewModel {
+    
+}
---
+++ RxMemo/ViewModel/MemoListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MemoListViewModel.swift
+//  RxMemo
+//
+//  Created by dwKang on 2022/07/01.
+//
+
+import Foundation
+
+class MemoListViewModel {
+    
+}
