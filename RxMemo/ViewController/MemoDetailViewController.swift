3a749e4c2de865fe215611a5f5b29622aac7c453
dongwoo kang <ehddn3821@naver.com>
2022-09-22 14:50:54 +0900

    Add MemoDetailViewModel and binding

--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class MemoDetailViewController: UIViewController, ViewModelBindableType {
     
@@ -17,7 +20,25 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
     @IBOutlet weak var shareButton: UIBarButtonItem!
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        viewModel.contents
+            .bind(to: contentTableView.rx.items) { tableView, row, value in
+                switch row {
+                case 0:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "contentCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                case 1:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                default:
+                    fatalError()
+                }
+            }.disposed(by: rx.disposeBag)
     }
 
     override func viewDidLoad() {
